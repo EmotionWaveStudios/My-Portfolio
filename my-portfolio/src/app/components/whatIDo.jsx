5d0967be4c17a3fe287cd251b5bd29f8302f0fc8
Mzwandile <mzwandilethatojunior@gmail.com>
2023-09-25 22:49:53 +0200

    project setup

--- my-portfolio/src/app/components/whatIDo.jsx
+++ my-portfolio/src/app/components/whatIDo.jsx
@@ -4,70 +4,66 @@ const WhatIDo = () => {
       <hr className="my-[50px]" />
       <h1 className="text-[#FF014F] text-5xl pb-[50px]">What I Do</h1>
 
-      <div className="flex justify-evenly gap-10">
-        <div id="block" className="">
+      <div className="flex flex-wrap gap-12">
+        <div id="block" className="w-[30%] rounded-lg shadow-xl shadow-[#33363a]">
           <div></div>
-          <h2>Business Strategy</h2>
-          <p>
+          <h2 className="px-4 py-2 text-2xl">Business Strategy</h2>
+          <p className="p-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
             repellat quo corrupti nemo quibusdam tempore et?
           </p>
         </div>
 
-        <div id="block">
+        <div id="block" className="w-[30%] rounded-lg shadow-xl shadow-[#33363a]">
           <div></div>
-          <h2>Business Strategy</h2>
-          <p>
+          <h2 className="px-4 py-2 text-2xl">Business Strategy</h2>
+          <p className="p-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
             repellat quo corrupti nemo quibusdam tempore et?
           </p>
         </div>
 
-        <div id="block">
+        <div id="block" className="w-[30%] rounded-lg shadow-xl shadow-[#33363a]">
           <div></div>
-          <h2>Business Strategy</h2>
-          <p>
+          <h2 className="px-4 py-2 text-2xl">Business Strategy</h2>
+          <p className="p-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
             repellat quo corrupti nemo quibusdam tempore et?
           </p>
         </div>
 
-        <div id="block">
+        <div id="block" className="w-[30%] rounded-lg shadow-xl shadow-[#33363a]">
           <div></div>
-          <h2>Business Strategy</h2>
-          <p>
+          <h2 className="px-4 py-2 text-2xl">Business Strategy</h2>
+          <p className="p-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
             repellat quo corrupti nemo quibusdam tempore et?
           </p>
         </div>
 
-        <div id="block">
+        <div id="block" className="w-[30%] rounded-lg shadow-xl shadow-[#33363a]">
           <div></div>
-          <h2>Business Strategy</h2>
-          <p>
+          <h2 className="px-4 py-2 text-2xl">Business Strategy</h2>
+          <p className="p-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
             repellat quo corrupti nemo quibusdam tempore et?
           </p>
         </div>
 
-        <div id="block">
+        <div id="block" className="w-[30%] rounded-lg shadow-xl shadow-[#33363a]">
           <div></div>
-          <h2>Business Strategy</h2>
-          <p>
+          <h2 className="px-4 py-2 text-2xl">Business Strategy</h2>
+          <p className="p-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
             repellat quo corrupti nemo quibusdam tempore et?
           </p>
         </div>
 
-        <div id="block">
-          <div></div>
-          <h2>Business Strategy</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
-            repellat quo corrupti nemo quibusdam tempore et?
-          </p>
-        </div>
+
       </div>
+
+      <hr className="my-[50px]" />
+
     </div>
   );
 };
